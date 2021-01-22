4a59e9c3a34a1d71e819e98dd2972ac0473d895a
admin <huor97ka@gmail.com>
2021-01-22 11:10:21 +0100

    updated project change file and class V4

---
+++ Clavier/doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Clavier","l":"Clavier()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Clavier","l":"lireFloat()"},{"p":"<Unnamed>","c":"Clavier","l":"lireInt()"},{"p":"<Unnamed>","c":"Clavier","l":"lireString()"},{"p":"<Unnamed>","c":"Clavier","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
